e406710ecbc0699d971595ac99571c49105030f5
VandeurenGlenn <vandeurenglenn@gmail.com>
2018-04-13 23:36:45 +0200

    :metal: Test & deploy with Travis

--- test.js
+++ test.js
@@ -5,3 +5,21 @@ test('repo config', tape => {
   tape.plan(1);
   config().then(({ repo, spec }) => tape.ok(Boolean(repo && spec)))
 });
+
+test('repo config with sharding & filestore enabled', tape => {
+  tape.plan(1);
+  config({
+    sharding: true,
+    filestore: true
+  }).then(({ repo, spec }) => tape.ok(
+    Boolean(
+      repo.Experimental.ShardingEnabled && repo.Experimental.FilestoreEnabled
+    )))
+});
+
+test('repo config with bootstrap for earth', tape => {
+  tape.plan(1);
+  config({
+    bootstrapFor: 'earth'
+  }).then(({ repo, spec }) => tape.ok(Boolean(repo.Bootstrap.length === 13)))
+});
