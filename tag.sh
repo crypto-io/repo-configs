a33c731d97d6a60e7d0fa6d7c99fab7fdcf3fa58
Glenn Vandeuren <vandeurenglenn@gmail.com>
2019-04-15 23:46:35 +0200

    :neutral_face: fix push

--- tag.sh
+++ tag.sh
@@ -1,3 +1,3 @@
 PACKAGE_VERSION=$(node -pe "require('./package.json').version")
 git tag -a "v$PACKAGE_VERSION" -m "version $PACKAGE_VERSION"
-git push v$PACKAGE_VERSION
+git push origin v$PACKAGE_VERSION
